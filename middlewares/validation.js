2d8f6e9af3e3950575957064e094fb3dcd30cdbb
Lesia Savchenko <les@Air-Lesia.local>
2023-06-15 08:22:56 +0300

    исправлен movieId в валидации

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -43,7 +43,7 @@ module.exports.saveMovieValidation = celebrate({
 
 module.exports.deleteMovieValidation = celebrate({
   params: Joi.object().keys({
-    _id: Joi.string().length(24).hex(),
+    movieId: Joi.string().length(24).hex(),
   }),
 });
 
